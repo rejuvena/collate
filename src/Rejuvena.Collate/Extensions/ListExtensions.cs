9cb36df76147b22ff7c3d03117da09f76ee95790
Tomat <xxlennygamerxx@gmail.com>
2022-08-24 18:54:37 -0700

    Whoops, fixed mods being added to enabled.json multiple times.

--- src/Rejuvena.Collate/Extensions/ListExtensions.cs
+++ src/Rejuvena.Collate/Extensions/ListExtensions.cs
@@ -5,8 +5,12 @@ namespace Rejuvena.Collate.Extensions
     public static class ListExtensions
     {
         public static List<T> With<T>(this List<T> list, T obj) {
-            list.Add(obj);
+            list.AddOnce(obj);
             return list;
+        }
+
+        public static void AddOnce<T>(this List<T> list, T obj) {
+            if (!list.Contains(obj)) list.Add(obj);
         }
     }
 }
