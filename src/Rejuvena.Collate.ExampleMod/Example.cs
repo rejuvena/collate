20f7f16544ee4bd77b5686cfd2ad25f36f655c86
Tomat <xxlennygamerxx@gmail.com>
2022-12-06 22:32:34 -0800

    NuGet package example.

--- src/Rejuvena.Collate.ExampleMod/Example.cs
+++ src/Rejuvena.Collate.ExampleMod/Example.cs
@@ -1,7 +1,17 @@
-﻿using Terraria.ModLoader;
+﻿using HtmlAgilityPack;
+using Terraria.ModLoader;
 
 namespace Rejuvena.Collate.ExampleMod;
 
 public class Example : Mod
 {
+    public override void Load() {
+        base.Load();
+        
+        Logger.Info("Hello from Collate!");
+
+        var web = new HtmlWeb();
+        var doc = web.Load("https://tmodloader.net");
+        Logger.Info(doc.ToString());
+    }
 }
